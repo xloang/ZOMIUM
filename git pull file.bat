3bf77e40fb4a38bed5d02bff4e8126e9cedc2c44
xloang <xloang25@gmail.com>
2026-04-20 21:19:53 +0300

    some updates

---
+++ git pull file.bat
@@ -0,0 +1,14 @@
+@echo off
+title Git Pull Tool
+
+echo =========================
+echo     GIT PULL TOOL
+echo =========================
+
+echo.
+echo [1] Github'dan cekiliyor...
+git pull origin main
+
+echo.
+echo Islem tamamlandi!
+pause
